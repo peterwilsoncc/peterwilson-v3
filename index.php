b597ac02e5caa3d54e38dcb8fe76b809beb4dadb
Peter Wilson <peter@hmn.md>
2016-09-11 11:52:10 +1000

    Configure WP to run from sub-directory.

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * WordPress bootstrapper
+ */
+define( 'WP_USE_THEMES', true );
+require( './wp/wp-blog-header.php' );
---
+++ wp-config.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Use an uncommitted local config file when not on Pantheon.
+ */
+
+if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) && file_exists( __DIR__ . '/wp-local-config.php' ) ) {
+	// Local config, not on Patheon.
+	require_once( __DIR__ . '/wp-local-config.php' );
+} elseif ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
+	// On Pantheon.
+	/**
+	 * @TODO: configure Pantheon.
+	 */
+} else {
+	// No local config, no Pantheon.
+}
+
+/**
+ * Standard WordPress configuration.
+ */
+
+// Database settings.
+$table_prefix  = 'wp_';
+defined( 'DB_CHARSET' ) || define( 'DB_CHARSET', 'utf8' );
+defined( 'DB_COLLATE' ) || define( 'DB_COLLATE', '' );
+
+// Language.
+defined( 'WPLANG' ) || define( 'WPLANG', '' );
+
+/**
+ * Hide errors.
+ *
+ * Unless the local-config file says otherwise.
+ */
+defined( 'WP_DEBUG' )         || define( 'WP_DEBUG', false );
+defined( 'WP_DEBUG_DISPLAY' ) || define( 'WP_DEBUG_DISPLAY', false );
+defined( 'WP_DEBUG_LOG' )     || define( 'WP_DEBUG_LOG', false );
+
+
+
+
+/* That's all, stop editing! Happy Pressing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/wp/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
