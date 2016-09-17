461885be80d6465ec09179959a5b063a0106c146
Peter Wilson <peter@hmn.md>
2016-09-18 09:32:09 +1000

    Config for phpunit tests.

---
+++ .tests/phpunit/wp-tests-config.php
@@ -0,0 +1,34 @@
+<?php
+
+$_SERVER['HTTP_HOST'] = 'localhost';
+
+define( 'DB_NAME', 'wordpress_test' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'password' );
+define( 'DB_HOST', 'localhost' );
+$table_prefix = 'wptests_';
+
+define( 'WP_TESTS_DOMAIN', 'localhost' );
+define( 'WP_TESTS_EMAIL', 'admin@example.org' );
+define( 'WP_TESTS_TITLE', 'Test Blog' );
+
+define( 'WP_PHP_BINARY', 'php' );
+
+// Define Site URL: WordPress in a subdirectory.
+if ( ! defined( 'WP_SITEURL' ) )
+	define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/' );
+
+// Define Home URL
+if ( ! defined( 'WP_HOME' ) )
+	define( 'WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] );
+
+// Prevent editing of files through the admin.
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
+
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname(__FILE__) . '/../../' );
+
+// include_once( __DIR__ . '/../wp-config.php' );
