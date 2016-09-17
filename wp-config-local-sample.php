bfe48cea9fbc80c3c42ca455c06d36576eab9acc
Peter Wilson <peter@hmn.md>
2016-09-17 14:44:12 +1000

    Local config demo file.

---
+++ wp-config-local-sample.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Database config.
+ */
+defined( 'DB_NAME' )     || define( 'DB_NAME', 'wordpress' );
+defined( 'DB_USER' )     || define( 'DB_USER', 'wordpress' );
+defined( 'DB_PASSWORD' ) || define( 'DB_PASSWORD', 'vagrantpassword' );
+defined( 'DB_HOST' )     || define( 'DB_HOST', 'localhost' );
+
+/**
+ * Show errors.
+ */
+defined( 'WP_DEBUG' )         || define( 'WP_DEBUG', true );
+defined( 'WP_DEBUG_DISPLAY' ) || define( 'WP_DEBUG_DISPLAY', true );
+defined( 'WP_DEBUG_LOG' )     || define( 'WP_DEBUG_LOG', true );
