ecf6dfdf6addfd13e425dcee9c55e23ef9c96bdf
Peter Wilson <peter@hmn.md>
2016-11-06 15:34:12 +1100

    Add tests to ensure vendor plugins are available.

---
+++ .tests/phpunit/tests/plugins/test-class-pw-vendor-plugins-enabled.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Ensure the vendor plugins are enabled.
+ */
+class Test_PW_Vendor_Plugins_Enabled extends WP_UnitTestCase {
+
+	/**
+	 * JB's extended-cpts available.
+	 */
+	function test_extended_cpts() {
+		$this->assertTrue( function_exists( 'register_extended_post_type' ), 'Extended CPTs available.' );
+	}
+
+	/**
+	 * JB's extended-taxos available.
+	 */
+	function test_extended_taxos() {
+		$this->assertTrue( function_exists( 'register_extended_taxonomy' ), 'Extended Taxos available.' );
+	}
+
+	/**
+	 * JB's extended template parts available.
+	 */
+	function test_extended_template_parts() {
+		$this->assertTrue( function_exists( 'get_extended_template_part' ), 'Extended Template Parts available.' );
+	}
+}
