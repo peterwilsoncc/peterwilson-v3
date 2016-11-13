fb34ff9c2ed4efd8669d00f098a4c7d16e1c0ac0
Peter Wilson <peter@hmn.md>
2016-11-13 17:03:58 +1100

    Register presentation CPT.

---
+++ wp-content/plugins-mu/pwcc-speaking/inc/presentations.php
@@ -0,0 +1,31 @@
+<?php
+namespace PWCC\Speaking\Presentations;
+
+function init() {
+	register_cpt();
+}
+
+function register_cpt() {
+	$cpt_args = [
+		'public'        => true,
+		'menu_position' => 20,
+		'menu_icon'     => 'dashicons-megaphone',
+		'hierarchical'  => false,
+		'has_archive'   => true,
+		'query_var'     => 'presentations',
+		'supports'      => [
+			'title',
+			'editor',
+			'excerpt',
+			'revisions',
+		],
+	];
+
+	$cpt_names = [
+		'singular' => 'Presentation',
+		'plural'   => 'Presentations',
+		'slug'     => 'presentations',
+	];
+
+	register_extended_post_type( 'pwcc_presentation', $cpt_args, $cpt_names );
+}
