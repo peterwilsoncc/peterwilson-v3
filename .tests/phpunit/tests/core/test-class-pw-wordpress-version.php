2e1ae010f1e7b25ccc328d7fcfe3d701a5cbcaf2
Peter Wilson <peter@hmn.md>
2016-12-30 15:07:48 +1100

    Bump version test.

--- .tests/phpunit/tests/core/test-class-pw-wordpress-version.php
+++ .tests/phpunit/tests/core/test-class-pw-wordpress-version.php
@@ -2,7 +2,7 @@
 
 class PW_WordPress_Version extends WP_UnitTestCase {
 	function test_version() {
-		$expected = '4.6.1';
+		$expected = '4.7';
 		$actual   = get_bloginfo( 'version' );
 
 		$this->assertEquals( $expected, $actual );
