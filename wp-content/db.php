89a3655311687d770f5c4743828c407ce52b0c04
Peter Wilson <peter@hmn.md>
2017-01-08 14:04:17 +1100

    Fix QM dropin inclusions.

--- wp-content/db.php
+++ wp-content/db.php
@@ -1,5 +1,5 @@
 <?php
-if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) && 'live' === $_ENV['PANTHEON_ENVIRONMENT'] ) {
+if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) || 'live' !== $_ENV['PANTHEON_ENVIRONMENT'] ) {
 	if ( file_exists( __DIR__ . '/plugins/query-monitor/wp-content/db.php' ) ) {
 		require_once __DIR__ . '/plugins/query-monitor/wp-content/db.php';
 	}
