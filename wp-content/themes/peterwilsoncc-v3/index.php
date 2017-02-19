cfe6e43ce7cc935727480cafcea9e991c3809130
Peter Wilson <peter@hmn.md>
2017-02-19 18:13:38 +1100

    Remove unneeded files.

--- wp-content/themes/peterwilsoncc-v3/index.php
+++ wp-content/themes/peterwilsoncc-v3/index.php
@@ -1,15 +1,4 @@
 <?php
 wp_head();
 
-
-
-// PWCC\Handlebars\get_hb_template_part( 'organism/global/colours' );
-
-var_dump([
-	PWCC\Styleguide\get_files(),
-	PWCC\Styleguide\get_names(),
-	PWCC\Styleguide\get_urls(),
-]);
-
-
 wp_footer();
