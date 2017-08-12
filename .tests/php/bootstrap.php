08dd12c5d3884b1cb2396634c0a52cbeeca45247
Peter Wilson <git-commit-by@peterwilson.cc>
2017-08-12 17:37:17 +1000

    GH-42: Remove checking for environement variable.

--- .tests/php/bootstrap.php
+++ .tests/php/bootstrap.php
@@ -17,9 +17,9 @@ if ( getenv( 'WP_DEVELOP_DIR' ) ) {
 }
 $wp_tests_dir = $wp_develop_dir . '/tests/phpunit';
 
-if ( getenv( 'CI' ) ) {
+//if ( getenv( 'CI' ) ) {
 	$config_file_path = __DIR__;
-}
+//}
 
 require_once $wp_tests_dir . '/includes/functions.php';
 
