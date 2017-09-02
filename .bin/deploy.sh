cb29871fc568b863ab1385c9192794d12cd3fbc8
Peter Wilson <git-commit-by@peterwilson.cc>
2017-09-02 13:01:03 +1000

    Hotfix: Double quote commit message with $ENVIRONMENT_VARS.

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -4,7 +4,6 @@ set -ex
 
 cd ~
 git clone git@github.com:peterwilsoncc/peterwilson-v3-compiled.git
-pwd # Tech debt.
 cd ~/peterwilson-v3-compiled
 git checkout -B $CIRCLE_BRANCH
 mv .git ~/_git
@@ -15,5 +14,5 @@ mv ~/_git ./.git
 git config user.email "auto-deploy@peterwilson.cc"
 git config user.name "Auto deploy"
 git add -A .
-git diff --quiet && git diff --staged --quiet || git commit -am 'Deploy $CIRCLE_BRANCH - Build $CIRCLE_BUILD_NUM from $CIRCLE_SHA1'
+git diff --quiet && git diff --staged --quiet || git commit -am "Deploy $CIRCLE_BRANCH - Build $CIRCLE_BUILD_NUM from $CIRCLE_SHA1"
 git push -u origin $CIRCLE_BRANCH
