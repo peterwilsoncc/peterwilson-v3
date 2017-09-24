537220526fde7e7aa4b5b27eed541ecab0634b7f
Peter Wilson <peterwilsoncc@Peters-MacBook-Pro.local>
2017-09-24 17:58:57 +1000

    Disable checks for theme updates.
    This is of limited use as it only runs when the theme is active. If this theme is not active the site has larger problems.
    
    I could move the disabling the theme update check to a plugin, but then that would only run when the plugin was active. 🤷🏻‍♂️

--- content/themes/peter-wilson-2017/inc/namespace.php
+++ content/themes/peter-wilson-2017/inc/namespace.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package peterwilson-2017
- * @author Peter Wilson <wilson@peterwilson.cc>
+ * @package   peterwilson-2017
+ * @author    Peter Wilson <wilson@peterwilson.cc>
  * @copyright 2017 Peter Wilson
- * @license GPLv2
- * @version 1.0.0-alpha
+ * @license   GPLv2
+ * @version   1.0.0-alpha
  */
 
 namespace PWCC\PeterWilson2017;
@@ -15,5 +15,29 @@ namespace PWCC\PeterWilson2017;
  * Runs on the `after_setup_theme` hook.
  */
 function bootstrap() {
+	add_filter( 'http_request_args', __NAMESPACE__ . '\\disable_theme_checks', 10, 2 );
+}
 
+/**
+ * Prevent WP checking for theme updates.
+ *
+ * Ensures that WordPress does check for updates to this theme on WordPress.org.
+ *
+ * Runs on the hook `http_request_args`.
+ *
+ * @param array  $args The array of HTTP request args.
+ * @param string $url          The request URL.
+ * @return array The array of HTTP request args modified to remove this theme.
+ */
+function disable_theme_checks( array $args, string $url ) {
+	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check' ) ) {
+		return $args;
+	}
+
+	// Remove this theme from the data sent to WP.org.
+	$themes = json_decode( $args['body']['themes'], true );
+	unset( $themes['themes']['peter-wilson-2017'] );
+	$args['body']['themes'] = wp_json_encode( $themes );
+
+	return $args;
 }
