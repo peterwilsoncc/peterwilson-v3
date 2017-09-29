3c3f8b605dafb8cdb00ae756dbf649e77e1b260b
Peter Wilson <peterwilsoncc@Peters-MacBook-Pro.local>
2017-09-29 14:18:54 +1000

    MVP Header.

--- content/themes/peter-wilson-2017/header.php
+++ content/themes/peter-wilson-2017/header.php
@@ -7,3 +7,15 @@
  * @copyright 2017 Peter Wilson
  * @license   GPLv2
  */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> <?php body_class( [] ); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+<body>
