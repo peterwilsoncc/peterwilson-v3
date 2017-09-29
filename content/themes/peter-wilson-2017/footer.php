b1acf4d04ce6841db1e1b45788f1c245b3aaaf39
Peter Wilson <peterwilsoncc@Peters-MacBook-Pro.local>
2017-09-29 14:03:15 +1000

    Basic header and footer file structure.

--- content/themes/peter-wilson-2017/footer.php
+++ content/themes/peter-wilson-2017/footer.php
@@ -1,15 +1,9 @@
 <?php
 /**
- * The theme index file.
+ * The theme footer file.
  *
  * @package   peterwilson-2017
  * @author    Peter Wilson <wilson@peterwilson.cc>
  * @copyright 2017 Peter Wilson
  * @license   GPLv2
  */
-
-get_header();
-?>
-
-<?php
-get_footer();
