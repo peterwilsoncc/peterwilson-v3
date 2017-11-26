e4a88212a6996c62b137eff606581ea835d0e985
Peter Wilson <peterwilson@Peters-HM-Laptop.local>
2017-11-26 15:28:37 -0500

    HTML for content layout.

--- content/themes/peter-wilson-2017/singular.php
+++ content/themes/peter-wilson-2017/singular.php
@@ -9,10 +9,27 @@
  */
 
 get_header();
+/*
+ * Get the post if it's set.
+ *
+ * I'm not doing anything special here if it doesn't because it should, OK,
+ * it just should. If it doesn't and execution reaches this file we have much
+ * bigger problems. OK? OK!
+ */
+have_posts() && the_post();
 ?>
 <div class="Page_SectionLead">
 </div>
-<main class="Page_Main Main">
+<main <?php post_class( 'Page_Main Main Article' ); ?>>
+	<div class="Main_Lead Article_Lead">
+		<h1 class="Headline entry-title">
+			<?php the_title(); ?>
+		</h1>
+	</div>
+	<div class="Main_Body Article_Body entry-content">
+		<?php the_content(); ?>
+	</div>
+	<?php get_sidebar(); ?>
 </main>
 <div class="Page_SectionFollow">
 </div>
