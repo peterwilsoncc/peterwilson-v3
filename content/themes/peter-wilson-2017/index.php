ae3776d0ccd8ab405c2bb06b33ae89291323a330
Peter Wilson <git-commit-by@peterwilson.cc>
2017-12-28 16:00:14 +1100

    Give our pages a title.

--- content/themes/peter-wilson-2017/index.php
+++ content/themes/peter-wilson-2017/index.php
@@ -11,6 +11,18 @@
 get_header();
 ?>
 <div class="Page_SectionLead">
+	<?php
+	switch ( true ) {
+		case is_archive() :
+			the_archive_title( '<h1 class="PageHeader">', '</h1>' );
+			break;
+		case is_home() && ! is_front_page() :
+			echo '<p class="PageHeader">';
+			single_post_title();
+			echo '</p>';
+			break;
+	}
+	?>
 </div>
 <?php
 $main_classes = [ 'Page_Main', 'Main' ];
